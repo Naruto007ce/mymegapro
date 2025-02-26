a7464e3cce954a513eeeef4fd4fd1468a8814547
Naruto007ce <nikammithilesh09@gmail.com>
2025-02-27 00:19:33 +0530

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flipkart Navbar Clone</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav class="navbar">
+        <div class="logo">
+            <a href="#"><img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/fkheaderlogo_exploreplus-44005d.svg" alt="Flipkart Logo"></a>
+        </div>
+
+        <div class="search-box">
+            <input type="text" placeholder="Search for Products, Brands and More">
+            <button>Search</button>
+        </div>
+
+        <div class="nav-links">
+            <a href="#">Login</a>
+            <a href="#">Become a Seller</a>
+            <a href="#">More</a>
+            <a href="#">Cart</a>
+        </div>
+    </nav>
+
+</body>
+</html>
